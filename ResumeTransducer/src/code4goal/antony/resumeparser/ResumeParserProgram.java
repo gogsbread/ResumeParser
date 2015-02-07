fa29fc410be9a47b86f3b0d83689d035a571f1cb
Antony Thomas <antonydeepak@gmail.com>
2015-02-07 03:20:35 -0800

    Completed all work

--- ResumeTransducer/src/code4goal/antony/resumeparser/ResumeParserProgram.java
+++ ResumeTransducer/src/code4goal/antony/resumeparser/ResumeParserProgram.java
@@ -14,6 +14,7 @@ import static gate.Utils.*;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -25,9 +26,14 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.tika.config.TikaConfig;
+import org.apache.tika.detect.Detector;
 import org.apache.tika.exception.TikaException;
+import org.apache.tika.io.TikaInputStream;
 import org.apache.tika.metadata.Metadata;
+import org.apache.tika.mime.MediaType;
 import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.sax.BodyContentHandler;
 import org.apache.tika.sax.ToXMLContentHandler;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -35,37 +41,34 @@ import org.json.simple.parser.JSONParser;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang.StringUtils;
 
 import com.google.gson.JsonObject;
 
 public class ResumeParserProgram {
-	public static void writeJson(String file) {
-		JSONObject json = new JSONObject();
-		json.put("title", "Harry Potter and Half Blood Prince");
-		json.put("author", "J. K. Rolling");
-		json.put("price", 20);
-		JSONArray jsonArray = new JSONArray();
-		jsonArray.add("Harry");
-		jsonArray.add("Ron");
-		jsonArray.add("Hermoinee");
-		json.put("characters", jsonArray);
-		try {
-			System.out.println("Writting JSON into file ...");
-			System.out.println(json);
-			FileWriter jsonFileWriter = new FileWriter(file);
-			jsonFileWriter.write(json.toJSONString());
-			jsonFileWriter.flush();
-			jsonFileWriter.close();
-			System.out.println("Done");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
 	private static File parseToHTMLUsingApacheTikka(String file)
 			throws IOException, SAXException, TikaException {
-		String OUTPUT_FILE_NAME = FilenameUtils.removeExtension(file) + ".html";
+		// determine extension
+		String ext = FilenameUtils.getExtension(file);
+		String outputFileFormat = "";
+		// ContentHandler handler;
+		if (ext.equalsIgnoreCase("html") | ext.equalsIgnoreCase("pdf")
+				| ext.equalsIgnoreCase("doc") | ext.equalsIgnoreCase("docx")) {
+			outputFileFormat = ".html";
+			// handler = new ToXMLContentHandler();
+		} else if (ext.equalsIgnoreCase("txt") | ext.equalsIgnoreCase("rtf")) {
+			outputFileFormat = ".txt";
+		} else {
+			System.out.println("Input format of the file " + file
+					+ " is not supported.");
+			return null;
+		}
+		String OUTPUT_FILE_NAME = FilenameUtils.removeExtension(file)
+				+ outputFileFormat;
 		ContentHandler handler = new ToXMLContentHandler();
+		// ContentHandler handler = new BodyContentHandler();
+		// ContentHandler handler = new BodyContentHandler(
+		// new ToXMLContentHandler());
 		InputStream stream = new FileInputStream(file);
 		AutoDetectParser parser = new AutoDetectParser();
 		Metadata metadata = new Metadata();
@@ -112,7 +115,9 @@ public class ResumeParserProgram {
 		Iterator iter = corpus.iterator();
 		JSONObject parsedJSON = new JSONObject();
 		Out.prln("Started parsing...");
-		while (iter.hasNext()) {
+		// while (iter.hasNext()) {
+		if (iter.hasNext()) { // should technically be while but I am just
+								// dealing with one document
 			JSONObject profileJSON = new JSONObject();
 			Document doc = (Document) iter.next();
 			AnnotationSet defaultAnnotSet = doc.getAnnotations();
@@ -133,7 +138,7 @@ public class ResumeParserProgram {
 
 				// Needed name Features
 				JSONObject nameJson = new JSONObject();
-				String[] nameFeatures = new String[] {"firstName",
+				String[] nameFeatures = new String[] { "firstName",
 						"middleName", "surname" };
 				for (String feature : nameFeatures) {
 					String s = (String) currAnnot.getFeatures().get(feature);
@@ -165,21 +170,90 @@ public class ResumeParserProgram {
 				curAnnSet = defaultAnnotSet.get(annSection);
 				it = curAnnSet.iterator();
 				JSONArray sectionArray = new JSONArray();
-				while (it.hasNext()) { // extract all values for each address,email,phone etc..
+				while (it.hasNext()) { // extract all values for each
+										// address,email,phone etc..
 					currAnnot = (Annotation) it.next();
 					String s = stringFor(doc, currAnnot);
 					if (s != null && s.length() > 0) {
 						sectionArray.add(s);
 					}
 				}
-				if(sectionArray.size() > 0){
-					profileJSON.put(annKeys[i],sectionArray);
+				if (sectionArray.size() > 0) {
+					profileJSON.put(annKeys[i], sectionArray);
 				}
 			}
-			if(!profileJSON.isEmpty()){
+			if (!profileJSON.isEmpty()) {
 				parsedJSON.put("basics", profileJSON);
 			}
-		}//while
+
+			// awards,credibility,education_and_training,extracurricular,misc,skills,summary
+			String[] otherSections = new String[] { "summary",
+					"education_and_training", "skills", "accomplishments",
+					"awards", "credibility", "extracurricular", "misc" };
+			for (String otherSection : otherSections) {
+				curAnnSet = defaultAnnotSet.get(otherSection);
+				it = curAnnSet.iterator();
+				JSONArray subSections = new JSONArray();
+				while (it.hasNext()) {
+					JSONObject subSection = new JSONObject();
+					currAnnot = (Annotation) it.next();
+					String key = (String) currAnnot.getFeatures().get(
+							"sectionHeading");
+					String value = stringFor(doc, currAnnot);
+					if (!StringUtils.isBlank(key)
+							&& !StringUtils.isBlank(value)) {
+						subSection.put(key, value);
+					}
+					if (!subSection.isEmpty()) {
+						subSections.add(subSection);
+					}
+				}
+				if (!subSections.isEmpty()) {
+					parsedJSON.put(otherSection, subSections);
+				}
+			}
+
+			// work_experience
+			curAnnSet = defaultAnnotSet.get("work_experience");
+			it = curAnnSet.iterator();
+			JSONArray workExperiences = new JSONArray();
+			while (it.hasNext()) {
+				JSONObject workExperience = new JSONObject();
+				currAnnot = (Annotation) it.next();
+				String key = (String) currAnnot.getFeatures().get(
+						"sectionHeading");
+				if (key.equals("work_experience_marker")) {
+					// JSONObject details = new JSONObject();
+					String[] annotations = new String[] { "date_start",
+							"date_end", "jobtitle", "organization" };
+					for (String annotation : annotations) {
+						String v = (String) currAnnot.getFeatures().get(
+								annotation);
+						if (!StringUtils.isBlank(v)) {
+							// details.put(annotation, v);
+							workExperience.put(annotation, v);
+						}
+					}
+					// if (!details.isEmpty()) {
+					// workExperience.put("work_details", details);
+					// }
+					key = "text";
+
+				}
+				String value = stringFor(doc, currAnnot);
+				if (!StringUtils.isBlank(key) && !StringUtils.isBlank(value)) {
+					workExperience.put(key, value);
+				}
+				if (!workExperience.isEmpty()) {
+					workExperiences.add(workExperience);
+				}
+
+			}
+			if (!workExperiences.isEmpty()) {
+				parsedJSON.put("work_experience", workExperiences);
+			}
+
+		}// if
 		Out.prln("Completed parsing...");
 		return parsedJSON;
 	}
@@ -191,19 +265,25 @@ public class ResumeParserProgram {
 			return;
 		}
 		String inputFileName = args[0];
-		String outputFileName = (args.length == 2) ? args[1] : "sample.out";
-		// writeJson(outputFileName);
+		String outputFileName = (args.length == 2) ? args[1]
+				: "parsed_resume.json";
+
 		try {
 			File tikkaConvertedFile = parseToHTMLUsingApacheTikka(inputFileName);
-			JSONObject parsedJSON = loadGateAndAnnie(tikkaConvertedFile);
-			
-			FileWriter jsonFileWriter = new FileWriter(outputFileName);
-			jsonFileWriter.write(parsedJSON.toJSONString());
-			jsonFileWriter.flush();
-			jsonFileWriter.close();
-			
+			if (tikkaConvertedFile != null) {
+				JSONObject parsedJSON = loadGateAndAnnie(tikkaConvertedFile);
+
+				Out.prln("Writing to output...");
+				FileWriter jsonFileWriter = new FileWriter(outputFileName);
+				jsonFileWriter.write(parsedJSON.toJSONString());
+				jsonFileWriter.flush();
+				jsonFileWriter.close();
+				Out.prln("Output written to file " + outputFileName);
+			}
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
+			System.out.println("Sad Face :( .Something went wrong.");
 			e.printStackTrace();
 		}
 	}
